d8d10c894b44cc828e6238ad3d44bc5c62564138
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2021-03-06 18:05:21 +1100

    Update

--- index.js
+++ index.js
@@ -14,25 +14,29 @@ app.get("/week.png", (req, res) => {
 app.get("/image/:random.png", (req, res) => {
     const canvas = createCanvas(50, 50);
     const ctx = canvas.getContext("2d");
-    const week = l.DateTime.local().setZone("Australia/Sydney").minus({weeks: 32}).plus({days: 2}).toFormat("W");
-    console.log(week);
 
+    const sem_start = l.DateTime.local(2021, 2, 22).setZone("Australia/Sydney");
+    const now = l.DateTime.local().setZone("Australia/Sydney")
+    const sem_millis = now.diff(sem_start).milliseconds;
+    const week = Math.ceil(sem_millis / (7 * 24 * 60 * 60 * 1000)).toString();
+
+    console.log(week);
 
     ctx.font = "34px Roboto";
     ctx.fillStyle = "white";
 
-	if (Number(week) < 13) {
-		ctx.fillText(week, 5, 35);
-	} else {
-		ctx.fillText("no", 5, 35);
-	}
+    if (Number(week) < 13) {
+        ctx.fillText(week, 15, 35);
+    } else {
+        ctx.fillText("no", 5, 35);
+    }
 
     res.set("Cache-Control", "no-store");
     res.set("Expires", "0");
     res.end(canvas.toBuffer(), "binary");
 });
 
-app.listen(port, "0.0.0.0", () =>
+app.listen(port, "0.0.0.0", () => {
     console.log(`listening on http://localhost:${port}`)
-);
+});
 
