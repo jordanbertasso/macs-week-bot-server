4633ae45f88167805bcad628c50b007ddb9a0d7f
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2021-04-19 11:37:03 +1000

    Add second half of sem bool

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 8080;
 
 let random = "";
 
+const second_half = true;
+
 app.get("/week.png", (req, res) => {
     random = Math.random().toString(36).substring(7);
     res.redirect(`/image/${random}.png`);
@@ -16,9 +18,9 @@ app.get("/image/:random.png", (req, res) => {
     const ctx = canvas.getContext("2d");
 
     const sem_start = l.DateTime.local(2021, 2, 20).setZone("Australia/Sydney");
-    const now = l.DateTime.local().setZone("Australia/Sydney")
+    const now = l.DateTime.local().setZone("Australia/Sydney");
     const sem_millis = now.diff(sem_start).milliseconds;
-    const week = Math.ceil(sem_millis / (7 * 24 * 60 * 60 * 1000)).toString();
+    const week = Math.ceil(sem_millis / (7 * 24 * 60 * 60 * 1000) - (second_half ? 2 : 0)).toString();
 
     console.log(week);
 
